e00308fd4e49c02dd126f9a90d46fa66b204241d
EC2 Default User <ec2-user@ip-172-31-31-12.us-east-2.compute.internal>
2019-08-01 15:49:36 +0000

    updated july 31

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,14 @@
 from app import app
 from flask import render_template, request
 from app.models import model, formopener
+from flask_googlemaps import GoogleMaps
+from flask_googlemaps import Map
+
+# you can set key as config
+app.config['GOOGLEMAPS_KEY'] = "AIzaSyDpEknfmsqyL1xulZ9M56HTM-yTn4Mzg8o"
+
+# Initialize the extension
+GoogleMaps(app)
 
 bmiNum = 0
 weight_range = 'normal weight'
@@ -35,10 +43,44 @@ def diet():
     global bmiNum
     global weight_range
     return render_template('diet.html', bmiNum = bmiNum, weight_range = weight_range)
+
 @app.route('/exercise', methods = ['GET','POST'])
 def exercise():
     global bmiNum
     global weight_range
     return render_template('exercise.html', bmiNum = bmiNum, weight_range = weight_range)
+    
+@app.route('/consistency', methods = ['GET','POST'])
+def consistency():
+    # creating a map in the view
+    mymap = Map(
+        identifier="view-side",
+        lat=37.4419,
+        lng=-122.1419,
+        markers=[(37.4419, -122.1419)]
+    )
+    sndmap = Map(
+        identifier="sndmap",
+        lat=40.7552,
+        lng=-73.9741,
+        markers=[
+          {
+             'icon': 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+             'lat':40.7552,
+             'lng':-73.9741,
+             'infobox': "<b>Hello World</b>"
+          },
+          {
+             'icon': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
+             'lat':40.7552,
+             'lng':-73.9741,
+             'infobox': "<b>Hello World from other place</b>"
+          }
+        ]
+    )
+    return render_template('consist.html', mymap=mymap, sndmap=sndmap)
+
+if __name__ == "__main__":
+    app.run(debug=True)
     
     
